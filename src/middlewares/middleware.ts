9b9e989d1aa4fbc9a9ad2905d14557ccfa695ade
tanaymishra <tanaymishra2204@gmail.com>
2025-12-14 17:20:22 +0530

    perfect Function orchastration

---
+++ src/middlewares/middleware.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+export interface UserPayload {
+    id: string;
+    email: string;
+    role: "company-admin" | "super-admin" | "company-employee";
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: UserPayload;
+        }
+    }
+}
+
+//Middleware for authentication JWT
+export const authMiddleware = (roles: UserPayload["role"][]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization?.split(" ")[1];
+            if (!token) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+
+            const decoded = verify(token, process.env.JWT_SECRET as string) as UserPayload;
+
+            // Attach user info to request object
+            req.user = decoded;
+
+            if (!roles.includes(decoded.role)) {
+                return res.status(403).json({ message: "Forbidden: Insufficient permissions" });
+            }
+
+            next();
+        } catch (error) {
+            return res.status(403).json({ message: "Forbidden: Invalid or expired token" });
+        }
+    }
+}
--- src/routes/user/auth/signup.ts
+++ src/routes/user/auth/signup.ts
@@ -1,2 +1,14 @@
 import { asyncQuery } from "../../../../config/database";
+import { Request } from "express";
+import { Response } from "express";
+export async function signup(req: Request, res: Response) {
+    try {
+        const { name, email, password } = req.body;
+        if (!req.user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
 
+    } catch (error) {
+
+    }
+}
